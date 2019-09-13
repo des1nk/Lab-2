8cc67ebb6b4a180ba21774b5746c01a6aab91ad4
kosmo1337 <54933914+kosmo1337@users.noreply.github.com>
2019-09-13 11:19:54 +0300

    Add files via upload

---
+++ лаба 2.1/лаба 2.cpp
@@ -0,0 +1,21 @@
+﻿// лаба 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int x1, x2, y1, y2;
+	cout << "x1=";
+	cin >> x1;
+	cout << "y1=";
+	cin >> y1;
+	cout << "x2=";
+	cin >> x2;
+	cout << "y2=";
+	cin >> y2;
+	cout << sqrt(pow(abs(x2-x1),2)+pow(abs(y2-y1),2));
+	
+}
+
---
+++ лаба 2.2/лаба 2.2.cpp
@@ -0,0 +1,21 @@
+﻿// лаба 2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int A, B, C, AC, BC;
+	cout << "A=";
+	cin >> A;
+	cout << "B=";
+	cin >> B;
+	cout << "C=";
+	cin >> C;
+	AC = abs(C - A);
+	BC = abs(C - B);
+	cout << "AC=" << AC << "\n";
+	cout << "BC=" << BC << "\n";
+	cout << "AC+BC=" << AC + BC << "\n";
+}
